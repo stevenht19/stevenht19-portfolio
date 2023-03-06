8ce3d51854ed91fa81dd8848856653a83d004f01
stevenht19 <mstvdev19@gmail.com>
2023-03-05 22:13:47 -0500

    add sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact({ compat: true })]
+  integrations: [tailwind(), preact({
+    compat: true
+  }), image({ serviceEntryPoint: '@astrojs/image/sharp' })]
 });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,9 @@ export default function Header() {
   const onToggle = () => setIsOpen(b => !b)
 
   return (
-    <header class='fixed w-full max-w-6xl translate-x-[-50%] left-1/2'>
-      <nav class='relative top-0 flex justify-between items-center py-4 md:py-7'>
-        <span class='text-slate-200 font-bold ml-8'>
-          STV
-        </span>
-        <button id='menu' class='mr-8 p-2' onClick={onToggle}>
+    <header class='absolute top-0 w-full'>
+      <nav class='relative flex max-w-6xl w-full mx-auto justify-end items-center py-4 md:py-7'>
+        <button id='menu' class='mr-8 p-2 md:hidden' onClick={onToggle} aria-label='menu'>
           {
             !open ? (
               <svg
@@ -19,7 +16,7 @@ export default function Header() {
                 width='24'
                 height='24'
                 viewBox='0 0 24 24'
-                class='fill-white md:hidden'
+                class='fill-white'
               >
                 <path d='M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z'></path>
               </svg>
@@ -29,7 +26,7 @@ export default function Header() {
                 width='24'
                 height='24'
                 viewBox='0 0 24 24'
-                class='fill-white md:hidden'
+                class='fill-white'
               >
                 <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z">
                 </path>
@@ -37,12 +34,20 @@ export default function Header() {
             )
           }
         </button>
-        <div class={`${!open ? 'hidden' : ''} absolute top-full backdrop-blur-xl w-full h-screen md:static md:backdrop-blur-none md:w-auto md:h-auto md:block`}>
-          <ul class='flex flex-col font-medium text-gray-200 px-8 pt-7 gap-8 md:flex-row md:gap-12 md:pt-0'>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Contact</li>
-            <li>Resume</li>
+        <div class={`${!open ? 'hidden' : ''} z-20 backdrop-blur-3xl absolute top-full w-full h-screen md:static md:backdrop-blur-none md:w-auto md:h-auto md:block`}>
+          <ul class='flex flex-col font-medium text-gray-300 px-8 pt-7 gap-8 md:flex-row md:gap-12 md:pt-0'>
+            <li class='hover:text-blue-100'>
+              <a href='#'>Home</a>
+            </li>
+            <li class='hover:text-blue-100'>
+              <a href='#projects'>Projects</a>
+            </li>
+            <li class='hover:text-blue-100'>
+              <a href='#contact'>Contact</a>
+            </li>
+            <li class='hover:text-blue-100'>
+              <a href='#'>Resume</a>
+            </li>
           </ul>
         </div>
       </nav>
